4ae9a1a4c79f730ab903f62e4e3a46f0f254fc65
Mathieu Dembinski <54134753+matdmb@users.noreply.github.com>
2025-01-25 14:39:21 +0100

    Adding more unit tests

--- internal/exifutils.go
+++ internal/exifutils.go
@@ -1,28 +1,38 @@
 package internal
 
 import (
+	"errors"
 	"os"
 	"time"
 
 	"github.com/rwcarlsen/goexif/exif"
 )
 
-func GetExifDate(path string) (time.Time, error) {
-	file, err := os.Open(path)
+// GetExifDate retrieves the EXIF date from a file using the provided decode and dateTime functions.
+func GetExifDate(
+	filePath string,
+	decodeFunc func(f *os.File) (*exif.Exif, error),
+	dateTimeFunc func(exifData *exif.Exif) (time.Time, error),
+) (time.Time, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
 		return time.Time{}, err
 	}
 	defer file.Close()
 
-	exifData, err := exif.Decode(file)
+	exifData, err := decodeFunc(file)
 	if err != nil {
-		return time.Time{}, err
+		return time.Time{}, errors.New("failed to decode EXIF data")
 	}
 
-	date, err := exifData.DateTime()
+	date, err := dateTimeFunc(exifData)
 	if err != nil {
-		return time.Time{}, err
+		return time.Time{}, errors.New("failed to retrieve EXIF date")
 	}
 
 	return date, nil
+}
+
+func decodeExifFromFile(f *os.File) (*exif.Exif, error) {
+	return exif.Decode(f)
 }
